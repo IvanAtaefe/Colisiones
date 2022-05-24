166a61ff92ec60fb0ba08169c134ec977e3ad51c
IvanAtaefe <88833116+IvanAtaefe@users.noreply.github.com>
2022-05-24 15:57:33 -0300

    Controles y Colisiones

---
+++ Assets/Scripts/CollisionTest.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    void OnCollisionEnter()
+    {
+        Debug.Log("Contacto");
+    }
+
+    void OnCollisionExit()
+    {
+        Debug.Log("Contacton't");
+    }
+
+    void OnCollisionStay()
+    {
+        Debug.Log("In Contacto");
+    }
+}
---
+++ Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "DeathWall" || col.gameObject.name == "DeathWall (1)" || col.gameObject.name == "DeathWall (2)" || col.gameObject.name == "DeathWall (3)") {
+            transform.position = new Vector3(0, 0.5f, 0);
+            transform.eulerAngles = new Vector3(0, 0, 0);
+        }
+    }
+
+    void OnCollisionExit(Collision col)
+    {
+           transform.position = new Vector3(0, 0.5f, 0);
+            transform.eulerAngles = new Vector3(0, 0, 0);
+    }
+}
